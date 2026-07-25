ed5c7986534f6b0af12918ba22ce04c71fc23acd
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-25 21:50:35 +0530

    Added Union of two Arrays

--- 07-Arrays/src/com/Arrays/ArrayListDemo.java
+++ 07-Arrays/src/com/Arrays/ArrayListDemo.java
@@ -1,6 +1,7 @@
 package com.Arrays;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
@@ -30,6 +31,21 @@ public class ArrayListDemo {
         listtwo.set(0,"nice");
         System.out.println(listtwo);   //[nice, sid, hello]
 
+        ArrayList<Integer> newList = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
 
+// to add items inside ArrayList using for loop:
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("enter the value to add: ");
+            newList.add(input.nextInt());
+        }
+
+//  to print ArrayList using for loop:
+
+        for (int i = 0; i < 5; i ++){
+            System.out.println("Enter the index position: ");
+            System.out.println(newList.get(input.nextInt()));
+        }
     }
 }
---
+++ 07-Arrays/src/com/Arrays/UnionOfArray.java
@@ -0,0 +1,26 @@
+package com.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class UnionOfArray {
+    static void main(String[] args) {
+        int[] arr1 = {1,2,9,2,4,8,3};
+        int[] arr2 = {1,2,3,4,5,6};
+        union(arr1,arr2);
+    }
+    static void union(int[] nums1, int [] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums1) {
+            set.add(i);
+        }
+        for(int i : nums2) {
+            set.add(i);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : set) {
+            list.add(i);
+        }
+        System.out.println(list);
+    }
+}
