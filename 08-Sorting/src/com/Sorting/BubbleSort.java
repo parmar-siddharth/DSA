56902999c2cea4c32a8467c8a8081c93d73dd3b3
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-27 23:33:28 +0530

    Added bubble Sort

--- 08-Sorting/src/com/Sorting/BubbleSort.java
+++ 08-Sorting/src/com/Sorting/BubbleSort.java
@@ -3,34 +3,26 @@ package com.Sorting;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int [] arr = {2,0,2,1,1,0};
-//        int [] arr = {1,2,3,4,5,6};
-        arr = bubbleSort(arr);
+    static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        bubbleSort(arr);
+    }
+    static void bubbleSort(int[] arr){
+        int n =  arr.length;
+        for(int i=0;i<n;i++){
+            boolean flag = false;
+            for(int j=1;j <= n-i-1;j++){
+                if(arr[j-1]>arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    flag = true;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
         System.out.println(Arrays.toString(arr));
     }
-   static int[] bubbleSort(int[] arr){
-        int n = arr.length;
-       for (int i = 0; i < n; i++) {
-           boolean isSwapped = false;
-           for (int j = 1; j < n-i; j++) {
-               if (arr[j] < arr[j-1]){
-                   int temp = arr[j];
-                   arr[j] = arr[j-1];
-                   arr[j-1] = temp;
-                   isSwapped = true;
-               }
-           }
-          // System.out.println(isSwapped);
-           if (!isSwapped){
-               break;
-           }
-       }
-       return arr;
-   }
 }
-/*
-Time complexity:
-Best Case : O(N)
-Worst case : O(N^2)
- */
