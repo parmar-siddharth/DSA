27d2cca0d0023a1fc7120f72226fa2c767d6538c
Siddharth <siddharthparmar.dev@gmail.com>
2026-08-07 23:58:56 +0530

    Added Spiral Matrix

---
+++ 07-Arrays/src/com/Arrays/SpiralMatrix.java
@@ -0,0 +1,37 @@
+package com.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    static void main(String[] args) {
+        int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        System.out.println(spiralOrder(matrix));
+    }
+    static List<Integer> spiralOrder(int[][] matrix) {
+        int top = 0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (top <= bottom && left <= right){
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                list.add(matrix[top][i]);
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                list.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for (int i = bottom; i >= top ; i--) {
+                list.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return list;
+    }
+}
