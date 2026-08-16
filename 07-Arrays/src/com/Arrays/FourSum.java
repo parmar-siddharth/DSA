c675887083c764906d885c43a7255f5fd6146fe6
Siddharth <siddharthparmar.dev@gmail.com>
2026-08-16 22:55:10 +0530

    Added 4 Sum

---
+++ 07-Arrays/src/com/Arrays/FourSum.java
@@ -0,0 +1,42 @@
+package com.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    static void main(String[] args) {
+
+    }
+    static List<List<Integer>> fourSum(int[] nums, int target){
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans  = new ArrayList<>();
+        for (int i = 0; i < n-3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < n-2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l){
+                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum < target){
+                        k++;
+                    }else if (sum > target) {
+                        l--;
+                    }else {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k-1]){
+                            k++;
+                        }while (k < l && nums[l] == nums[l+1]){
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
