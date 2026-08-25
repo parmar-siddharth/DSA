d57655b27ed32f6eaca674becff65f397ec26c69
Siddharth <siddharthparmar.dev@gmail.com>
2026-08-25 23:55:50 +0530

    added KoKo eating bananas

---
+++ 10-BinarySearch/src/com/BinarySearch/KokoEatingBananas.java
@@ -0,0 +1,36 @@
+package com.BinarySearch;
+
+public class KokoEatingBananas {
+    static void main(String[] args) {
+        int[] piles = {30,11,23,4,20};
+        System.out.println(minEatingSpeed(piles,5));
+    }
+    static int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        for (int num : piles){
+            max = Math.max(max,num);
+        }
+        int low = 1;
+        int high = max;
+        int ans = -1;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if (canFinish(piles,mid,h)){
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low =  mid + 1;
+            }
+        }
+        return ans;
+    }
+    static boolean canFinish(int[] piles, int k, int h){
+        long hours = 0;
+        for (int pile : piles){
+            hours += pile / k;
+            if (pile % k != 0) hours++;
+        }
+        return hours <= h;
+    }
+}
